9a857b00b723419ab4b393fd0171c06ca0f2c304
TemurEShtemirov <temureshtemirov10@gmail.com>
2024-04-11 23:37:50 +0500

    added dino main and css and images

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Dino from './components/Dino/Dino'
+
+export default function App() {
+  return (
+    <>
+    <div className="App">
+      <Dino/>
+    </div>
+    </>
+  )
+}
